af2702eefa13426c365fd130d9b01d4917419825
liuchaowd <xiaonongyue@gmail.com>
2019-07-04 08:18:10 +0800

    algo

--- bucket_count_radix.c
+++ bucket_count_radix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void print(int a[], int size) {
   int i = 0;
@@ -44,6 +45,42 @@ void count_sort(int a[], int size) {
   free(count);
 }
 
+void radix_sort(int a[], int n, int bite_num) {
+  int i;
+
+  int radix = 10;
+  int j;
+  int* count = (int*)malloc(10 * 4);
+  int* res = (int*)malloc(n * 4);
+  int index;
+  for (i = 0; i < bite_num; ++i) {
+    radix = pow(10, i);
+    memset(count, 0, 4 * 10);
+
+    for (j = 0; j < n; j++) {
+      index = (a[j] / radix) % 10;
+      count[index]++;
+    }
+
+    for(j = 1; j < 10; j++) {
+      count[j] += count[j - 1];
+    }
+
+    for (j = n - 1; j >= 0; j--) {
+      index = (a[j] / radix) % 10;
+      res[count[index] - 1] = a[j];
+      count[index] --;
+    }
+    memcpy(a, res, 4 * n);
+  }
+}
+
+void radix_test() {
+  int a[10] = {123,12341,1232134,124,236,128,1112313129,98,9,8989};
+  radix_sort(a, 10, 10);
+  print(a, 10);
+}
+
 int main() {
   int a [10]={1,5,6,8,10,9,3,1,2,1};
   count_sort(a,10);
@@ -52,4 +89,6 @@ int main() {
     printf("val %d ", a[i]);
   }
   printf("\n");
+
+  radix_test();
 }
